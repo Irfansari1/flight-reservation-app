468325209298db360baa0f21a158a8e5db03e44d
Irfansari1 <irfansari42@gmail.com>
2022-03-02 21:30:58 +0100

    FRA auth and userapp added

--- main/urls.py
+++ main/urls.py
@@ -33,4 +33,7 @@ urlpatterns = [
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path('__debug__/', include('debug_toolbar.urls')),
+
+    #apps urls
+    path('users/', include('users.urls')),
 ]
